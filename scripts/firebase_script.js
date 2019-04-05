303394c2cafaddf0ca4ec349597ee2f52f4e028e
Luke Hansen <lukejohnhansen@gmail.com>
2019-04-04 18:05:01 -0700

    organized the file structure and made seperate css / js / html folders

---
+++ scripts/firebase_script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyArQsuHet0HsvqAmZJlT7xoq4ztZI_bcp4",
+	authDomain: "commshopp-8fabb.firebaseapp.com",
+	databaseURL: "https://commshopp-8fabb.firebaseio.com",
+	projectId: "commshopp-8fabb",
+	storageBucket: "commshopp-8fabb.appspot.com",
+	messagingSenderId: "279282392039"
+};
+firebase.initializeApp(config);
