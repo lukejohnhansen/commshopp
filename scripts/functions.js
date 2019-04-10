9525952957459b1f7294f43bc1ce8714e9f0bea3
JeffreyK <jeffrey_kuo113@hotmail.com>
2019-04-10 15:04:26 -0700

    updated some stuff

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,24 +1,17 @@
 $(document).ready(function() {
 	console.log("ready");
-for (let i = 1; i <= 3; i++){
 $('form').submit(function(event){
+for (var i = 1; i < 3; i++){
+	event.preventDefault();
 											console.log("clicked");
 											
 var quantity = $('#item'+i+'OrderQ').val();
-	
 	console.log(quantity);
 	var db = firebase.database().ref("items/item"+i);
-	
-	db.on ("value",function(snap){
-		   obj = snap.val();
-		   console.log(obj);
-		   console.log(obj["Quantity"]);   //quantity
-		   console.log(obj["Name"]);       //name
-	});
-	
 	db.update({
 	"Quantity": quantity
 	});
-})
 }
-});
+}
+)}
+									);
