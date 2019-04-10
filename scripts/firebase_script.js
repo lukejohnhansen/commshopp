a4e9c2a93ebd6d710209b6275e5ebb053c81ba94
Gina Kim <kimjuk1230@gmail.com>
2019-04-10 15:10:03 -0700

    getting email of user

--- scripts/firebase_script.js
+++ scripts/firebase_script.js
@@ -1,11 +1,27 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyArQsuHet0HsvqAmZJlT7xoq4ztZI_bcp4",
-	authDomain: "commshopp-8fabb.firebaseapp.com",
-	databaseURL: "https://commshopp-8fabb.firebaseio.com",
-	projectId: "commshopp-8fabb",
-	storageBucket: "commshopp-8fabb.appspot.com",
-	messagingSenderId: "279282392039"
+  apiKey: "AIzaSyArQsuHet0HsvqAmZJlT7xoq4ztZI_bcp4",
+  authDomain: "commshopp-8fabb.firebaseapp.com",
+  databaseURL: "https://commshopp-8fabb.firebaseio.com",
+  projectId: "commshopp-8fabb",
+  storageBucket: "commshopp-8fabb.appspot.com",
+  messagingSenderId: "279282392039"
 };
 firebase.initializeApp(config);
+
+
+
+
+    var firebase = app_firebase;
+
+    // for the current user
+    // create this user node in the datebase
+
+    firebase.auth().onAuthStateChanged(function(user){
+        firebase.database().ref("users/"+user.uid).update(
+    {
+        "name":user.displayName,
+         "email":user.email
+        });
+    });
 
